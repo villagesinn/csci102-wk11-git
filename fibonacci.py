e8f5ec4a784647b9f5bd59765cd8cdcafb044d09
Jianna West <west@mymail.mines.edu>
2019-11-12 10:10:49 -0700

    created file that calculates the first 10 Fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+# fibonacci.py
+
+def fib():
+    fibs = [1, 2]
+
+    for i in range(1,9):
+        new = fibs[i] + fibs[i-1]
+        fibs.append(new)
+        ''' 
+        implement Fibonacci sequence to calculate the 
+        first 10 Fibonacci numbers, note Fn = Fn-1 + Fn-2
+        '''
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
